8acbe0ee090b4113cea0e6a335d1f507deba2727
wy373226722 <a@12345679.cn>
2016-04-29 12:55:59 +0800

    v0.0.1

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,55 @@
-new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue.js!'
-    }
-})
+$('document').ready(function () {
+    var showAuthorInfo = function () {
+        console.log("showAuthorInfo");
+        new Vue({
+            el: '.app',
+            data: {
+                message: 'Hello Vue.js!'
+            }
+        });
+    };
+    var listBooks = function () {
+        console.log("listBooks");
+    };
+
+    var viewBook = function (bookId) {
+        console.log("viewBook: bookId is populated: " + bookId);
+    };
+
+    var allroutes = function () {
+        var route = window.location.hash.slice(2);
+        var sections = $('section');
+        var section;
+        
+        section = sections.filter('[data-route=' + route + ']');
+        if (section.length) {
+            sections.hide(200);
+            section.show(200);
+        }else{
+            console.log("not found.");
+        }
+    };
+
+    //
+    // define the routing table.
+    //
+    var routes = {
+        '/author': showAuthorInfo,
+        '/books': listBooks,
+        '/books/view/:bookId': viewBook
+    };
+
+    //
+    // instantiate the router.
+    //
+    var router = Router(routes);
+
+    //
+    // a global configuration setting.
+    //
+    router.configure({
+        on: allroutes
+    });
+
+    router.init();
+});
